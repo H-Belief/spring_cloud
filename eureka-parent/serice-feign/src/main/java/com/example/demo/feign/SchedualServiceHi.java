b322d5596a4804f998532fd7c1980c37158d6f5c
huguoyu <296667184@qq.com>
2017-12-10 13:01:23 +0800

    eureka、fegin、ribbon、zuul、config Demo

---
+++ eureka-parent/serice-feign/src/main/java/com/example/demo/feign/SchedualServiceHi.java
@@ -0,0 +1,16 @@
+package com.example.demo.feign;
+
+import com.example.demo.feign.hystrix.SchedualServiceHiHystric;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by HGY on 2017-11-25.
+ */
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
